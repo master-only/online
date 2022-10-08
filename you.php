c5a0616b127824f61a5474d79f599212c9e48ae2
master-only <109359453+master-only@users.noreply.github.com>
2022-10-09 06:29:45 +0700

    Update you.php

---
+++ you.php
@@ -0,0 +1,22 @@
+{"Version":"1.5",
+
+"Url":"https://github.com/droperos/atcglub/raw/main/config.zip" ,
+
+"Changelog":"  
+
+อัพเดทไฟล์ล่าสุด กด ตกลง
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
